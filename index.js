31b71d9fa654a421d5d6d6ba074b794f2f5a11ea
Chukwuemeka Torti <emekatorti@gmail.com>
2024-12-10 08:58:15 -0500

    removed some log statements

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.get('/styles.css', (inRequest, inResponse) => {
 })
 
 app.post('/register', (inRequest, inResponse) => {
-  console.log('Data sent from client', inRequest.body);
-
   const responseObj = {
     title: inRequest.body.title,
     first_name: inRequest.body.first_name,
@@ -23,49 +21,8 @@ app.post('/register', (inRequest, inResponse) => {
     username: inRequest.body.username,
     age: inRequest.body.age
   };
-  console.log('Response object', responseObj);
   inResponse.send(responseObj)
 });
-
-
-//set html to call this endpoint with the METHOD attribute of html form 
-//add the urlencoded middleware to the express app
-// app.post('/registerBody', (req, res) => {
-//     res.send(`<html>
-//         <head>
-//         <title style="border: 5px solid black;">Registration Successful</title>
-//         <style>
-//             th { background-color: #a0e0e0}
-//             tr { border: 1px solid #a0a0a0}
-//             tr { border: 1px solid #a0a0a0}
-//         </style>
-//         </head>
-//         <body>
-//             <h2>Thanks for registering</h2>
-//             <table>
-//             <tr>
-//             <th>Title</th>
-//             <th>First Name</th>
-//             <th>Last Name</th>
-//             <th>Email</th>
-//             <th>Phone Number</th>
-//             <th>Username</th>
-//             <th>Age</th>
-//             </tr>
-//             <tr>
-//              <td>${req.body.title}</td>
-//              <td>${req.body.first_name}</td>
-//              <td>${req.body.last_name}</td>
-//              <td>${req.body.email_address}</td>
-//              <td>${req.body.phone_number}</td>
-//              <td>${req.body.username}</td>
-//              <td>${req.body.age}</td>  <!-- Assuming age is a number -->
-//              </tr>
-//             </table>
-//             </tr>
-//             </body>
-//         </html>`)
-// });
 
 app.listen(80, function () {
   console.log('Server is listening on port 80')
